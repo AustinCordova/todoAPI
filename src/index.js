7089fddf3e8383c5707c75a269090c9c8d9fe281
FosterCareAwareness <51935764+FosterCareAwareness@users.noreply.github.com>
2020-07-27 22:27:13 -0400

    API

--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Road2Hire',
-    database: 'todo'
+    database: 'ToDo'
 });
 
 try {
     connection.connect();
 } catch(e) {
-    console.log('Oops. Connection to MySQL failed.');
+    console.log('MySQL Connection failed.');
     console.log(e);
 }
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
 api.use(bodyParser.json());
-
 api.listen(3000, ()=>{
     console.log('API up and running!');
 });
@@ -27,7 +26,6 @@ api.listen(3000, ()=>{
 api.get('/tasks', (req, res) => {
     connection.query('SELECT * FROM tasks ORDER BY created DESC', (error, results) => {
         if (error) return res.json({ error: error });
-
         res.json(results);
     });
 });
@@ -35,10 +33,8 @@ api.get('/tasks', (req, res) => {
 api.post('/tasks/add', (req, res) => {
     connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
         if (error) return res.json({ error: error });
-
         connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
             if (error) return res.json({ error: error });
-
             res.json({
                 id: results[0]['LAST_INSERT_ID()'],
                 description: req.body.item
@@ -50,7 +46,6 @@ api.post('/tasks/add', (req, res) => {
 api.post('/tasks/:id/update', (req, res) => {
     connection.query('UPDATE tasks SET completed = ? WHERE id = ?', [req.body.completed, req.params.id], (error, results) => {
         if (error) return res.json({ error: error });
-
         res.json({});
     });
 });
@@ -58,7 +53,6 @@ api.post('/tasks/:id/update', (req, res) => {
 api.post('/tasks/:id/remove', (req, res) => {
     connection.query('DELETE FROM tasks WHERE id = ?', [req.params.id], (error, results) => {
         if (error) return res.json({ error: error });
-
         res.json({});
     });
 });
